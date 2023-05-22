a2da76080595171a82550bf022b18d61977fd907
Svetlana Korbut <tebba@yandex.ru>
2023-05-22 20:32:27 +0200

    fix:name of import path to root

--- Jobored/src/main.tsx
+++ Jobored/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Root from './routes/Root';
+import Root from './routes/root';
 import './index.css';
 import JobSearchPage from './routes/jobSearch_page';
 import ErrorPage from './routes/error-page';
